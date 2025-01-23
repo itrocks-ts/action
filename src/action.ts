79d7c37a0b209e06b9dbcc8fc20db116d39143e1
Baptiste Pillot <baptiste@pillot.fr>
2025-01-23 11:02:06 +0100

    Remove htmlTemplateResponse: redundant without its final implementation

--- src/action.ts
+++ src/action.ts
@@ -1,11 +1,6 @@
 import { HtmlResponse } from '@itrocks/core-responses'
 import { JsonResponse } from '@itrocks/core-responses'
 import { Headers }      from '@itrocks/request-response'
-import { Request }      from './request'
-
-type HtmlTemplateResponseFunction = (
-	data: any, request: Request, templateFile: string, statusCode?: number, headers?: Headers
-) => Promise<HtmlResponse>
 
 export class Action
 {
@@ -18,16 +13,6 @@ export class Action
 	jsonResponse(data: any, statusCode = 200, headers: Headers = {})
 	{
 		return new JsonResponse(data, statusCode, headers)
-	}
-
-	async htmlTemplateResponse(data: any, request: Request, templateFile: string, statusCode = 200, headers: Headers = {})
-	{
-		return this.htmlResponse(data.toString(), statusCode, headers)
-	}
-
-	static setHtmlTemplateResponse(htmlTemplateResponse: HtmlTemplateResponseFunction)
-	{
-		Action.prototype.htmlTemplateResponse = htmlTemplateResponse
 	}
 
 }
