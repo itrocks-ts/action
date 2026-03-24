5808264e1f4264adb42f23355e527306c406a3d1
Baptiste Pillot <baptiste@pillot.fr>
2026-03-24 16:37:28 +0100

    Add @Actions() inheritance support, including DEFAULT and CLEAR

--- src/actions.ts
+++ src/actions.ts
@@ -1,23 +1,51 @@
-import { ObjectOrType }          from '@itrocks/class-type'
-import { decorate, decoratorOf } from '@itrocks/decorator/class'
+import { ObjectOrType }   from '@itrocks/class-type'
+import { Type }           from '@itrocks/class-type'
+import { typeOf }         from '@itrocks/class-type'
+import { decorate }       from '@itrocks/decorator/class'
+import { ownDecoratorOf } from '@itrocks/decorator/class'
 
 const ACTIONS = Symbol('actions')
 
-const DEFAULT = ['add', 'delete', 'edit', 'json', 'list', 'output', 'save', 'summary']
+export const CLEAR   = Symbol('clear')
+export const DEFAULT = ['add', 'delete', 'edit', 'json', 'list', 'output', 'save', 'summary']
+
+type ActionValue = typeof CLEAR | string | readonly string[]
 
 let defaultActions = DEFAULT
 
-export function Actions(value: string[] = [])
+export function Actions(...actionValues: ActionValue[])
 {
-	return decorate(ACTIONS, value)
+	return (target: Type) => {
+		const parent  = Object.getPrototypeOf(target)
+		const actions = new Set((parent === Function.prototype) ? defaultActions : actionsOf(parent))
+		for (const value of actionValues) {
+			if (value === CLEAR) {
+				actions.clear()
+				continue
+			}
+			if (typeof value === 'string') {
+				actions.add(value)
+				continue
+			}
+			for (const valueEntry of value) {
+				actions.add(valueEntry)
+			}
+		}
+		return decorate(ACTIONS, [...actions])(target)
+	}
 }
 
 export function actionsOf(target: ObjectOrType)
 {
-	return decoratorOf(target, ACTIONS, defaultActions)
+	const actions = ownDecoratorOf<string[] | undefined>(target, ACTIONS, undefined)
+	if (actions) {
+		return actions
+	}
+	Actions()(typeOf(target))
+	return ownDecoratorOf<string[]>(target, ACTIONS, [])
 }
 
-export function setDefaultActions(actions: string[] = DEFAULT)
+export function setDefaultActions(actions: readonly string[] = DEFAULT)
 {
-	defaultActions = actions
+	defaultActions = [...actions]
 }
